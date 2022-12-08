4f379c4fccb74c358382ae1defd7b9e80bf9d1f9
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2022-12-09 02:39:02 +0900

    commit

--- board/src/main/java/org/zerock/mapper/BoardMapper.java
+++ board/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,11 +2,12 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.vo.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insertSelectKey(BoardVO vo);
 }
--- board/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ board/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.vo.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -21,5 +22,18 @@ public class BoardMapperTests {
 	public void testGetList() {
 		
 		mapper.getList().forEach(list -> log.info(list));
+	}
+	
+	@Test
+	public void testinsertSelectKey() {
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("입력한 제목");
+		vo.setContent("입력한 내용");
+		vo.setWriter("입력한 사람");
+		
+		mapper.insertSelectKey(vo);
+		
+		log.info(vo);
 	}
 }
