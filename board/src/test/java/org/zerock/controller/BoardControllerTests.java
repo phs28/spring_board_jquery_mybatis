1431e3ccd7993228858e642c2d6dd41fd2276b07
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2022-12-11 00:07:19 +0900

    commit

---
+++ board/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,19 @@
+package org.zerock.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({
+	
+})
+@Log4j
+public class BoardControllerTests {
+	
+	
+}
