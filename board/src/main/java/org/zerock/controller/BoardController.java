feb64eda48b9048c31cc7dd18d71f0f414a9f73a
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2022-12-12 21:48:18 +0900

    commit

--- board/src/main/java/org/zerock/controller/BoardController.java
+++ board/src/main/java/org/zerock/controller/BoardController.java
@@ -1,35 +1,81 @@
 package org.zerock.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.service.BoardService;
 import org.zerock.vo.BoardVO;
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @RestController
 @RequestMapping("/board/*")
 @Log4j
+@AllArgsConstructor
 public class BoardController {
 	
-	@Setter(onMethod_ = @Autowired)
 	private BoardService service;
 	
 	@GetMapping("/list")
 	public void getList(Model model) {
 		
-		List<BoardVO> list = service.getList();
 		
-		log.info(list);
 		
-		model.addAttribute("list", list);
+		log.info("list..");
+		
+		model.addAttribute("list", service.getList());
 	}
 	
+	@PostMapping("/register")
+	public String register(BoardVO vo, RedirectAttributes rttr) {
+		
+		log.info("BoardVO: " + vo);
+		
+		service.register(vo);
+		
+		rttr.addAttribute("result", vo.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void Get(@RequestParam("bno") long bno, Model model) {
+		
+		log.info("/get");
+		
+		BoardVO one = service.get(bno);
+		
+		model.addAttribute("board", one);
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO vo, RedirectAttributes rttr) {
+		
+		log.info("modify.." + vo);
+		
+		if(service.modify(vo)) {
+			rttr.addAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") long bno, RedirectAttributes rttr) {
+		
+		log.info("remove.." + bno);
+		
+		if(service.remove(bno)) {
+			rttr.addAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
 	
 }
--- board/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ board/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -1,19 +1,95 @@
 package org.zerock.controller;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration({
-	
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
 })
 @Log4j
 public class BoardControllerTests {
 	
+	@Setter(onMethod_ = @Autowired)
+	private WebApplicationContext ctx;
 	
+	private MockMvc mvc;
+	
+	@Before
+	public void setup() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception {
+		
+		log.info(
+				mvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getViewName()
+		);
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		
+		String resultPage = mvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 내용")
+				.param("writer", "user00")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info("register.." + resultPage);
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		
+		log.info(mvc.perform(MockMvcRequestBuilders
+				.get("/board/get")
+				.param("bno","5"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+		
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		
+		String resultPage = mvc.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "4")
+				.param("title", "테스트 수정 제목")
+				.param("content", "테스트 수정 내용")
+				.param("writer", "user01")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info("modify.." + resultPage);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		
+		String resultPage = mvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "4")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info("remove.." + resultPage);
+	}
+		
 }
